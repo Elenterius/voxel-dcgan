05a0f92ba82c98d0bc4a3b64bc4991c19aedc909
wangyida <yidawang.cn@gmail.com>
2018-02-05 11:28:18 +0100

    change dataset path

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ nsf = 4 # encoded voxel size, scale factor
 nvx = 32 # output voxel size
 batch_size = 64
 learning_rate = 2e-4
-dataset_path = "path/to/dataset/*.binvox"
+dataset_path = "/media/wangyida/D0-P1/database/ShapeNetCore.v2/*/*/*/model_normalized.binvox"
 params_path = "params/voxel_dcgan_model.ckpt"
